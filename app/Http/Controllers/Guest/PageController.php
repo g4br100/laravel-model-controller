891c9d733f1d93a67e0349f777bed206458eafa1
g4br100 <124401134+g4br100@users.noreply.github.com>
2023-07-14 20:07:43 +0200

    Fino a punto 7

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+
+        $links = config('store.someLinks');
+        return view('welcome', compact('links'));
+
+    }
+}
